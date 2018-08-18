ca309b1222639ad102e79fe66de56559c098bb3b
Markus Reitboeck <mrpilot@gmx.net>
2018-08-18 21:06:47 +0200

    (+) add QApplication name and organization info

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+    QApplication::setOrganizationName("mrpilot");
+    QApplication::setOrganizationDomain("http://github.com/mrpilot2");
+    QApplication::setApplicationName("rocrailBBTCalculator");
 
     return QApplication::exec();
 }
