3ebd741f559632f3089006a21137bf08f0ebca53
Markus Reitboeck <mrpilot@gmx.net>
2018-08-18 20:50:43 +0200

    (+) initial project setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    return QApplication::exec();
+}
