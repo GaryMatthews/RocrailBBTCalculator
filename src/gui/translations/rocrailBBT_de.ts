a0c4dc98b21238887703076c66b8530d13323501
Markus Reitboeck <mrpilot@gmx.net>
2018-09-09 14:13:18 +0200

    (~) update translation files

--- src/gui/translations/rocrailBBT_de.ts
+++ src/gui/translations/rocrailBBT_de.ts
@@ -2,6 +2,100 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_AT" sourcelanguage="en_US">
 <context>
+    <name>BBTModel</name>
+    <message>
+        <location filename="../bbtmodel.cpp" line="79"/>
+        <source>Block</source>
+        <translation>Block</translation>
+    </message>
+    <message>
+        <location filename="../bbtmodel.cpp" line="81"/>
+        <source>From Block</source>
+        <translation>Von Block</translation>
+    </message>
+    <message>
+        <location filename="../bbtmodel.cpp" line="83"/>
+        <source>Route</source>
+        <translation>Fahrstraße</translation>
+    </message>
+    <message>
+        <location filename="../bbtmodel.cpp" line="85"/>
+        <source>Deceleration time</source>
+        <translation>Abbremszeit</translation>
+    </message>
+    <message>
+        <location filename="../bbtmodel.cpp" line="87"/>
+        <source>Steps</source>
+        <translation>Schritte</translation>
+    </message>
+    <message>
+        <location filename="../bbtmodel.cpp" line="89"/>
+        <source>Speed</source>
+        <translation>Geschwindigkeit</translation>
+    </message>
+    <message>
+        <location filename="../bbtmodel.cpp" line="91"/>
+        <source>Counter</source>
+        <translation>Zähler</translation>
+    </message>
+    <message>
+        <location filename="../bbtmodel.cpp" line="93"/>
+        <source>Fixed</source>
+        <translation>Fest</translation>
+    </message>
+</context>
+<context>
+    <name>BlockModel</name>
+    <message>
+        <location filename="../blockmodel.cpp" line="69"/>
+        <source>Mainline</source>
+        <translation>Hauptlinie</translation>
+    </message>
+    <message>
+        <location filename="../blockmodel.cpp" line="71"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../blockmodel.cpp" line="73"/>
+        <source>Length</source>
+        <translation>Länge</translation>
+    </message>
+</context>
+<context>
+    <name>LocModel</name>
+    <message>
+        <location filename="../locmodel.cpp" line="79"/>
+        <source>BBT</source>
+        <translation>BBT</translation>
+    </message>
+    <message>
+        <location filename="../locmodel.cpp" line="81"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../locmodel.cpp" line="83"/>
+        <source>BBT Steps</source>
+        <translation>BBT Schritte</translation>
+    </message>
+    <message>
+        <location filename="../locmodel.cpp" line="85"/>
+        <source>V min</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../locmodel.cpp" line="87"/>
+        <source>V mid</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../locmodel.cpp" line="89"/>
+        <source>V cru</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="../forms/mainwindow.ui" line="25"/>
@@ -9,50 +103,142 @@
         <translation>Lokomotive</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="48"/>
+        <location filename="../forms/mainwindow.ui" line="52"/>
+        <source>Blocks &amp;&amp; Routes</source>
+        <translation>Blöcke &amp;&amp; Fahrstraßen</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="62"/>
+        <source>Blocks</source>
+        <translation>Blöcke</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="72"/>
+        <source>Routes</source>
+        <translation>Fahrstraßen</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="85"/>
+        <source>Show only mainline</source>
+        <translation>Zeige nur Hauptlinie</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="93"/>
         <source>BBT</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="57"/>
+        <location filename="../forms/mainwindow.ui" line="105"/>
+        <source>Minimum value: 0,5
+Maximum value: 1,5</source>
+        <translation>Minimaler Wert: 0,5
+Maximaler Wert: 1,5</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="128"/>
+        <source>Correction factor</source>
+        <translation>Korrekturfaktor</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="151"/>
         <source>Calculate</source>
         <translation>Berechnen</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="91"/>
+        <location filename="../forms/mainwindow.ui" line="173"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="106"/>
+        <location filename="../forms/mainwindow.ui" line="188"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="109"/>
-        <location filename="../forms/mainwindow.ui" line="112"/>
+        <location filename="../forms/mainwindow.ui" line="191"/>
+        <location filename="../forms/mainwindow.ui" line="194"/>
         <source>Quit</source>
         <translation>Beenden</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="115"/>
+        <location filename="../forms/mainwindow.ui" line="197"/>
         <source>Exit the application</source>
         <translation>Beendet die Applikation</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="120"/>
+        <location filename="../forms/mainwindow.ui" line="202"/>
         <source>&amp;Open workspace</source>
         <translation>&amp;Arbeitsbereich öffnen</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="123"/>
+        <location filename="../forms/mainwindow.ui" line="205"/>
         <source>Open workspace</source>
         <translation>Arbeitsbereich öffnen</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="126"/>
+        <location filename="../forms/mainwindow.ui" line="208"/>
         <source>Opens an existing Rocrail workspace</source>
         <translation>Öffnet einen existierenden Rocrail Arbeitsbereich</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <source>Select Rocrail workspace</source>
+        <translation>Rocrail Arbeitsbereich auswählen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="131"/>
+        <source>Workspace directory not found</source>
+        <translation>Arbeitsbereich nicht gefunden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="132"/>
+        <source>The selected workspace directory does notexist.</source>
+        <translation>Das ausgewählte Verzeichnis existiert nicht.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="195"/>
+        <source>entries</source>
+        <translation>Einträge</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="206"/>
+        <source>Existing BBT entries</source>
+        <translation>Existierende BBT Einträge</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="207"/>
+        <source>There are some BBT entries stored for this locomotive.
+Shall they be overwritten?</source>
+        <translation>Es existieren bereits BBT Einträge für diese Lokomotive.
+Sollen diese überschrieben werden?</translation>
+    </message>
+</context>
+<context>
+    <name>RouteModel</name>
+    <message>
+        <location filename="../routemodel.cpp" line="90"/>
+        <source>Mainline</source>
+        <translation>Hautplinie</translation>
+    </message>
+    <message>
+        <location filename="../routemodel.cpp" line="92"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../routemodel.cpp" line="94"/>
+        <source>From Block</source>
+        <translation>Von Block</translation>
+    </message>
+    <message>
+        <location filename="../routemodel.cpp" line="96"/>
+        <source>To Block</source>
+        <translation>Nach Block</translation>
+    </message>
+    <message>
+        <location filename="../routemodel.cpp" line="98"/>
+        <source>Straight</source>
+        <translation>Gerade</translation>
     </message>
 </context>
 </TS>
