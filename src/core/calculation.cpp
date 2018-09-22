6821c36879e5eb3abea870ce149bec77ad8bced8
Markus Reitboeck <mrpilot@gmx.net>
2018-09-22 11:47:37 +0200

    (*) for all routes that are not mainline only brake with v_mid

--- src/core/calculation.cpp
+++ src/core/calculation.cpp
@@ -17,41 +17,44 @@ void Calculation::calculateNewBBTEntries(double correctionFactor)
 {
     for (const auto& route : routes)
     {
+        BBT bbt;
+
+        bbt.route = route.id;
+        bbt.fromBlock = route.fromBlock;
+        bbt.block = route.toBlock;
+        bbt.count = 0;
+
         if (route.isOnlyMainline)
         {
-            BBT bbt;
-
-            bbt.route = route.id;
-            bbt.fromBlock = route.fromBlock;
-            bbt.block = route.toBlock;
-            bbt.count = 0;
-
             bbt.speed =
-                ! route.shallReduceVelocity
+                !route.shallReduceVelocity
                     ? loc->v_cru
                     : route.isCompletelyStraight ? loc->v_cru : loc->v_mid;
-
-            bbt.steps = loc->bbtSteps;
-
-            const double speedMmPerSecond{(bbt.speed - loc->v_min) / 3.6 * 1000};
-            const double speedMMPerSecondScaled{speedMmPerSecond /
-                                                H0ScalingFactor};
-
-            const QString blockName{bbt.block};
-            const auto search = [blockName](const Block& item) {
-                return item.name == blockName;
-            };
-
-            auto blockIt = std::find_if(blocks.begin(), blocks.end(), search);
-
-            if (blockIt != blocks.end())
-            {
-                bbt.interval = static_cast<int>(2.0 * blockIt->length /
-                                                speedMMPerSecondScaled / bbt.steps * 1000.0);
-                bbt.interval = static_cast<int>(bbt.interval * correctionFactor);
-            }
-
-            loc->bbt.emplace_back(bbt);
         }
+        else
+        {
+            bbt.speed = loc->v_mid;
+        }
+        bbt.steps = loc->bbtSteps;
+
+        const double speedMmPerSecond{(bbt.speed - loc->v_min) / 3.6 * 1000};
+        const double speedMMPerSecondScaled{speedMmPerSecond / H0ScalingFactor};
+
+        const QString blockName{bbt.block};
+        const auto search = [blockName](const Block& item) {
+            return item.name == blockName;
+        };
+
+        auto blockIt = std::find_if(blocks.begin(), blocks.end(), search);
+
+        if (blockIt != blocks.end())
+        {
+            bbt.interval =
+                static_cast<int>(2.0 * blockIt->length /
+                                 speedMMPerSecondScaled / bbt.steps * 1000.0);
+            bbt.interval = static_cast<int>(bbt.interval * correctionFactor);
+        }
+
+        loc->bbt.emplace_back(bbt);
     }
 }
