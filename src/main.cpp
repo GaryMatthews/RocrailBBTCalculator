f4e76861f5163b734d950e3684efcf9d27e08427
Markus Reitboeck <mrpilot@gmx.net>
2018-10-06 22:48:00 +0200

    (+) add a simple application icon

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char* argv[])
 
     Q_INIT_RESOURCE(application);
 
+    QApplication::setWindowIcon(QIcon(":/icons/appicon"));
     Core core{};
 
     MainWindowController controller{};
